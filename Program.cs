9bd0a704bc4bbc8e09792d372dd2ce3635739138
Xreality <JSHMorgan@outlook.com>
2023-04-24 14:28:28 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NugetTest;
+
+/// <summary>
+/// Main class for the program to run from.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Main saying hello world.
+    /// </summary>
+    /// <param name="args"></param>
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
